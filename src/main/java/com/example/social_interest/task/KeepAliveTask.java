d3543be798ed5862232cb34c2b49ce85a70e5101
deepakSingh023 <dsinghrana71@gmail.com>
2026-03-06 00:32:00 +0530

    health check fix

--- src/main/java/com/example/social_interest/task/KeepAliveTask.java
+++ src/main/java/com/example/social_interest/task/KeepAliveTask.java
@@ -1,6 +1,7 @@
 package com.example.social_interest.task;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -8,7 +9,8 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class KeepAliveTask {
 
-    private static final String PING_URL = "https://social-interest.onrender.com/api/health";
+    @Value("${health.uri}")
+    private String PING_URL;
 
     private final RestTemplate restTemplate = new RestTemplate();
 
